fb92bb8f7f43cdf0fb0593b3a21a442462306c0f
Arturo Suárez <manuel.suarez@cimat.mx>
2023-04-26 08:43:37 -0600

    Download data

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ import segmentation_models_pytorch as smp
 
 from pprint import pprint
 from torch.utils.data import DataLoader
+from segmentation_models_pytorch.datasets import SimpleOxfordPetDataset
+
+# download data
+root = "."
+SimpleOxfordPetDataset.download(root)
